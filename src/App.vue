87c840cbb5efd2cddb7545b8c26e2e48dbb3aafb
Gourish Vashisht <gourish.vashisht@nagarro.com>
2019-10-08 00:57:03 +0530

    Added bootstrap-vue plugin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="app">
-    
-    <router-view/>
+    <AppNavbar></AppNavbar>
+    <router-view />
+    <AppFooter></AppFooter>
   </div>
 </template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import { AppFooter, AppNavbar } from '@/components/shared';
+
+@Component({
+  components: {AppFooter, AppNavbar}
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  background-color : rgb(247, 230, 230);
+}
+.routes{
+  margin-top: 54px;
+  margin-bottom: 54px;
+}
+</style>
