366d14dbf667ad37954d0efa3bbe927e78d58def
Gourish Vashisht <gourish.vashisht@nagarro.com>
2019-10-08 00:57:56 +0530

    Added footer and navbar for application

---
+++ src/components/shared/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="light" fixed="bottom">
+      <b-collapse id="nav-collapse" is-nav>
+        <span>Add somethings here !</span>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <span>
+            Made with
+            <fa-icon class="footer-heart" icon="heart" />By Gourish Vashisht
+          </span>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style scoped>
+.footer-heart {
+  color: red;
+}
+</style>
---
+++ src/components/shared/AppNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-navbar class="app-header" toggleable="lg" type="dark" variant="primary" fixed="top">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+// import users from '../store/modules/users';
+
+@Component
+export default class AppHeader extends Vue {
+  public signoutUser() {
+    // users.setUser('');
+    this.$router.push('/login');
+  }
+}
+</script>
+
+<style scoped>
+.app-header {
+  box-shadow: 0.5px 2px 6px rgb(37, 36, 36);
+}
+</style>
