5dd528deb1587ca3dc5751f39cc15494649fb5bf
Gourish Vashisht <gourish.vashisht@nagarro.com>
2019-10-19 20:25:17 +0530

    Added unit test cases for TagList component

--- tests/unit/components/TagList.spec.ts
+++ tests/unit/components/TagList.spec.ts
@@ -1,5 +1,41 @@
+import { shallowMount, Wrapper } from "@vue/test-utils";
+import TagList from "@/components/common/TagList.vue";
+
 describe("TagList ", () => {
-  it("should run", () => {
-    expect(true).toBeTruthy();
+
+  const tagList = ["Tag1", "Tag2", "Tag3"];
+  let wrapper: Wrapper<TagList>;
+
+  function getTagListWrapper() {
+    return shallowMount(TagList, {
+      propsData: {
+        tags: tagList
+      }
+    });
+  }
+
+  beforeEach(() => {
+    wrapper = getTagListWrapper();
   });
+
+  it("should be created", () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("should display tags provided as props from parent component", () => {
+    expect((wrapper.findAll("li").length)).toEqual(3);
+    expect(wrapper.findAll("li").at(0).text()).toEqual("Tag1");
+    expect(wrapper.findAll("li").at(1).text()).toEqual("Tag2");
+    expect(wrapper.findAll("li").at(2).text()).toEqual("Tag3");
+  });
+
+  it("should display changed tags when props data is changed from parent component", () => {
+    wrapper.setProps({
+      tags: ["Tag1"]
+    });
+    expect(wrapper.text()).toEqual("Tag1");
+    expect((wrapper.findAll("li").length)).toEqual(1);
+    expect(wrapper.find("li").text()).toEqual("Tag1");
+  });
+
 });
